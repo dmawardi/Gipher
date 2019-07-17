877544c1b6dcef8398d520798422015bc99daf2e
Dannar Mawardi <dmawardi@gmail.com>
2019-07-17 22:48:46 +1000

    Index.html base structure created with external connections

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+var topics = ['Cyber Punk', 'Apex Legends', 'Fortnite', 'Devil May Cry', 'StarCraft', 'Diablo', 'Grand Theft Auto', 'Red Dead Redemption', 'Tomb Raider', 'Mario Kart', 'Super Mario', 'Final Fantasy'];
+
+function makeButtonsFrom(array) {
+    for (var i = 0; i < array.length; i++) {
+        
+    }
+}
