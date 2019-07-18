8ffafad37c8946e97ebd060f20109ab925619591
Dannar Mawardi <dmawardi@gmail.com>
2019-07-18 23:02:11 +1000

    Adding value to array functional for button creation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,13 @@
-var topics = ['Cyber Punk', 'Apex Legends', 'Fortnite', 'Devil May Cry', 'StarCraft', 'Diablo', 'Grand Theft Auto', 'Red Dead Redemption', 'Tomb Raider', 'Mario Kart', 'Super Mario', 'Final Fantasy'];
+var games = ['Cyber Punk', 'Apex Legends', 'Fortnite', 'Devil May Cry', 'StarCraft', 'Diablo', 'Grand Theft Auto', 'Red Dead Redemption', 'Tomb Raider', 'Mario Kart', 'Super Mario', 'Final Fantasy'];
 
 var apiKey = 'wqxHRPOli6qO50YxXqMbQ1wYnC3FPGhJ';
 var searchSyntax = 'api.giphy.com/v1/gifs/search?';
 
 
-// Takes in an array (ex. topics) and outputs each item as a button in the btnArea
-function makeButtonsFrom(array) {
+// Takes in an array (ex. games) and outputs each item as a button in the btnArea
+function RenderButtonsFrom(array) {
     var container = $('<div>');
     container.addClass('container  col-12');
-    console.log('container created');
 
     for (var i = 0; i < array.length; i++) {
         var button = $('<button>');
@@ -17,24 +16,59 @@ function makeButtonsFrom(array) {
         button.attr('data-topic', array[i]);
         button.text(array[i]);
         container.append(button);
-        console.log('button appended to container');
     }
 
     var buttonArea = $('#btnArea');
-    console.log(buttonArea);
 
+    // Empty and reappend the buttons to the button area
     buttonArea.empty();
-
     buttonArea.append(container);
 }
 
-function searchAPICall(searchTerm) {
-    $(ajax)
+// Function to convert titles for API Calls
+function TitleConverter(movieName) {
+    var searchTerm = '';
+    console.log(movieName.length);
+    for (var i = 0; i < movieName.length; i++) {
+        console.log('on cycle: '+i);
+        console.log(movieName[i]);
+        if (movieName[i] === ' ') {
+          searchTerm += '+';
+        } else {
+          searchTerm += movieName[i];
+        }
+    }
+    return searchTerm
+  }
+
+
+function SearchAPICall(searchTerm) {
+    $.ajax({
+        url: 'https://',
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+    });
 }
 
+// Event handler for topic buttons being clicked
 $('#topicButtons').on('click', function(){
 
 
-})
+});
 
-makeButtonsFrom(topics);
+// Event handler for adding value to button list
+$('#addGame').on('click', function(event) {
+    // Stops page refresh from input form submission
+    event.preventDefault();
+    // Take new game user input value submitted by user and assign: newGameName
+    var newGameName = $('#gameInput').val();
+
+    console.log(newGameName);
+
+    // Push new game to games array and re-render buttons from games
+    games.push(newGameName);
+    RenderButtonsFrom(games);
+});
+
+RenderButtonsFrom(games);
